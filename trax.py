14af64c175ea24b0495062b35089667f28c9dabc
ProfOak <OpenProfOak@gmail.com>
2016-03-20 17:18:20 -0500

    changed layout of DB table structure, minor changes
    Minor changes:
    Added comments
    Added safe checking of values

--- trax.py
+++ trax.py
@@ -7,6 +7,7 @@ import tabulate
 def process(line, db):
     ''' process the user input command '''
 
+    # case doesn't matter
     line = line.lower()
 
     if line == 'a':     # add
@@ -14,7 +15,7 @@ def process(line, db):
 
         print '=== New show ==='
         while title == '':
-            title   = raw_input('Title: ')
+            title   = raw_input('Title: ').strip()
 
         season  = get_int('Season  (default=1): ')
         episode = get_int('Episode (default=1): ')
@@ -23,19 +24,28 @@ def process(line, db):
                 VALUES (?, ?, ?)', (title, season, episode))
 
     elif line == 'r':   # remove
-        shows = call_db(db, 'select * from shows')
-        pretty_print(shows)
+        shows = pretty_print(db)
 
-        series_number = raw_input('Series number: ')
+        if len(shows) <= 0:
+            print 'No shows to remove'
+            return True
 
-        call_db(db, 'DELETE FROM shows WHERE ID = ?', series_number)
-        print 'Series deleted'
+        # be a number, and be in range
+        series_number = ''
+        while not series_number.isdigit() or \
+                int(series_number) >= len(shows) or \
+                int(series_number) < 0:
+            series_number = raw_input('Series number: ')
+
+        show_to_delete = shows[int(series_number)]
+
+        call_db(db, 'DELETE FROM shows WHERE title=? AND season=? AND  episode=?', show_to_delete)
+        print 'Series deleted:', show_to_delete[0]
         process('l', db)
 
 
     elif line == 'l':   # list
-        shows = call_db(db, 'select * from shows')
-        pretty_print(shows)
+        pretty_print(db)
 
     elif line == 'h':   # help
         usage()
@@ -54,6 +64,7 @@ def call_db(db, sql, vals=''):
     try:
         results = [ row for row in c.execute(sql, vals) ]
     except sqlite3.OperationalError:
+        # no table called 'shows' in db
         create_db(db)
         return
 
@@ -67,7 +78,6 @@ def create_db(db):
 
     call_db(db, '''
         create table shows(
-            ID INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT NOT NULL,
             season INTEGER NOT NULL,
             episode INTEGER NOT NULL
@@ -88,9 +98,15 @@ def get_int(string):
             return 1
     return n
 
-def pretty_print(table):
+def pretty_print(db):
     ''' nice printing using tabulate '''
-    print tabulate.tabulate(table, headers=('#', 'Title', 'Season', 'Episode'))
+
+    shows = call_db(db, 'SELECT * FROM shows')
+
+    # making i a str to use raw_input when making a selection
+    p_shows = ( [str(i)] + list(show) for i, show in enumerate(shows) )
+    print tabulate.tabulate(p_shows, headers=('#', 'Title', 'Season', 'Episode'))
+    return shows
 
 def usage():
     ''' usage displays the help command information '''
@@ -116,7 +132,7 @@ def main(db='trax.db',line=''):
         create_db(db)
 
     while process(line, db):
-        line = raw_input('> ')
+        line = raw_input('\n> ')
 
 if __name__ == '__main__':
     try:
